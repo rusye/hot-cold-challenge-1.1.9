2645492b0a9852148c098e1e8216facc22b8426b
Russ yev <ruslanyev@gmail.com>
2019-01-01 18:35:05 -0800

    Updates

---
+++ precache-manifest.f2fa6f137b1beb9a6a5e7fa7b22c98d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc910e9e2c1e02101f21",
+    "url": "/hot-cold-challenge-1.1.9/static/js/runtime~main.dc910e9e.js"
+  },
+  {
+    "revision": "93f83705a0f0aa6f62f7",
+    "url": "/hot-cold-challenge-1.1.9/static/js/main.93f83705.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/hot-cold-challenge-1.1.9/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "93f83705a0f0aa6f62f7",
+    "url": "/hot-cold-challenge-1.1.9/static/css/main.1606894b.chunk.css"
+  },
+  {
+    "revision": "bfc5ee7ded802c6caf0ee1f8207299e0",
+    "url": "/hot-cold-challenge-1.1.9/index.html"
+  }
+];
